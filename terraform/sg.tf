3b68be8386d4d6c2c4216095d1f8448cde835966
kkhoi <107262078+kkhoi@users.noreply.github.com>
2024-09-11 14:13:06 +0700

    Create sg.tf

---
+++ terraform/sg.tf
@@ -0,0 +1,37 @@
+#cluster
+resource "aws_security_group" "eks_cluster_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg-shoppingapp"
+  }
+}
+#node
+resource "aws_security_group" "eks_node_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-node-sg-shoppingapp"
+  }
+}
